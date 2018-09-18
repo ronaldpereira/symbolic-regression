dd5b32a0b3c7322d0fe03a6f48d135a6b76fe803
Ronald <ronald.drp11@gmail.com>
2018-09-18 11:52:28 -0300

    symbolic regression update

--- src/symbolic_regression.py
+++ src/symbolic_regression.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
-import math
+
 import tree
+import sys
+import fitness
+
+treeMaxDepth = 7
 
 class Individual:
-    
     def __init__(self):
+        self.tree = self.generateRandomTree()
+
+    def generateRandomTree(self):
         pass
+
 
 root = tree.Node('+')
 root.insert(5, 'l')
-root.insert(4, 'r')
+root.insert('-', 'r')
+root.right.insert(9, 'l')
+root.right.insert(3, 'r')
 root.print_subtree()
+print(end='\n\n')
+
+fit = fitness.Fitness('./input/datasets/synth1/synth1-train.csv')
+fit.calculate(root)
+fit = fitness.Fitness('./input/datasets/concrete/concrete-train.csv')
+fit.calculate(root)
