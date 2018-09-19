40ec2f793d1d618f2637cf9ecef3c8bec76be987
Ronald <ronald.drp11@gmail.com>
2018-09-19 11:54:27 -0300

    makefile creation and symbolic regression update

--- src/symbolic_regression.py
+++ src/symbolic_regression.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 
-import tree
 import sys
-import fitness
+import libs.data as data
+import libs.tree as tree
+import libs.fitness as fitness
+import math
 
 treeMaxDepth = 7
 
 class Individual:
-    def __init__(self):
-        self.tree = self.generateRandomTree()
+    def __init__(self, randomSeed=None):
+        self.root = self.generateRandomTree(randomSeed).root
 
-    def generateRandomTree(self):
-        pass
+    def generateRandomTree(self, randomSeed=None):
+        return tree.RandomTree(dataHolder.numberOfVariables, randomSeed)
 
+dataHolder = data.Data('./input/datasets/synth1/synth1-train.csv')
 
-root = tree.Node('+')
-root.insert(5, 'l')
-root.insert('-', 'r')
-root.right.insert(9, 'l')
-root.right.insert(3, 'r')
-root.print_subtree()
-print(end='\n\n')
+population = []
+for index in range(0, int(sys.argv[1])):
+    print(index)
+    population.append(Individual(index))
+    population[index].root.print_tree()
 
-fit = fitness.Fitness('./input/datasets/synth1/synth1-train.csv')
-fit.calculate(root)
-fit = fitness.Fitness('./input/datasets/concrete/concrete-train.csv')
-fit.calculate(root)
+fit = fitness.Fitness(dataHolder.train)
+#fit.calculate(ind.root)
+print(eval('((0.804694852981791)**(((math.cos(math.log((250)**(15.142228210831043))))+(math.sqrt(math.sin((76.3139001845522)/(11.011117825660167)))))*(91.99716498921566)))'))
